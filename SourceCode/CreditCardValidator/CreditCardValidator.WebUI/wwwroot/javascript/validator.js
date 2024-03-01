6e387aaaa337934b32854021404f390b018f7c75
AzureAD\NishanthanManchunath <nishanthan.manchunathan@cin7.com>
2024-03-01 14:18:36 +0530

    changes on the Popup UI.

--- SourceCode/CreditCardValidator/CreditCardValidator.WebUI/wwwroot/javascript/validator.js
+++ SourceCode/CreditCardValidator/CreditCardValidator.WebUI/wwwroot/javascript/validator.js
@@ -5,12 +5,15 @@
 
     let keyChar = String.fromCharCode(charCode);
 
-    if (/\d/.test(keyChar)) {
+    if (/\d/.test(keyChar))
+    {
         let value = input.value.replace(/\s/g, '');
         let formattedValue = '';
 
-        for (let i = 0; i < value.length; i++) {
-            if (i > 0 && i % 4 === 0) {
+        for (let i = 0; i < value.length; i++)
+        {
+            if (i > 0 && i % 4 === 0)
+            {
                 formattedValue += ' ';
             }
             formattedValue += value[i];
@@ -24,46 +27,52 @@
     return false;
 }
 
-const RESPONSE_STATUS = {
-    VALID: 1000,
-    INVALID: 1001
+const ResponseStatus = {
+    Valid: 1000,
+    Invalid: 1001
 }
 
-const RESPONSE_COLOR = {
-    VALID: "lightgreen",
-    INVALID: "orange",
-    ERROR: "red"
+const ValidationMessage = {
+    Empty: "Please enter card number..",
+    Valid: "Its a valid card..",
+    Invalid: "Its an invalid card..",
+    Error: "Sorry, Something went wrong.."
 }
 
-const VALIDATION_MESSAGE = {
-    EMPTY: "Enter card number.",
-    VALID: "Its a valid card.",
-    INVALID: "Its an invalid card."
+const PopupTitle = {
+    Valid: "Success..",
+    Invalid: "Sorry..",
+    Error: "Oops..",
+    Empty: "Warning"
 }
 
-const VALIDATION_MESSAGE_TIMEOUT = {
-    EMPTY_INPUT: 4000
+const PopupIcon = {
+    Valid: "success",
+    Invalid: "error",
+    Error: "error",
+    Empty: "warning"
 }
 
-function updateResponse(span, response)
+function popUpMessage(title, message, icon)
 {
-    if (response.status === RESPONSE_STATUS.VALID)
+    Swal.fire({
+        title: title,
+        text: message,
+        icon: icon
+    })
+}
+
+function updateResponse(response)
+{
+    if (response.status === ResponseStatus.Valid)
     {
         if (response.data)
-        {
-            span.style.background = RESPONSE_COLOR.VALID;
-            span.innerHTML = VALIDATION_MESSAGE.VALID;
-        } else
-        {
-            span.style.background = RESPONSE_COLOR.INVALID;
-            span.innerHTML = VALIDATION_MESSAGE.INVALID;
-        }
+            popUpMessage(PopupTitle.Valid, ValidationMessage.Valid, PopupIcon.Valid);
+        else
+            popUpMessage(PopupTitle.Invalid, ValidationMessage.Invalid, PopupIcon.Invalid)
+
     } else
-    {
-        span.style.background = RESPONSE_COLOR.ERROR;
-        span.innerHTML = response.message;
-    }
-    span.style.display = "flex";
+        popUpMessage(PopupTitle.Error, ValidationMessage.Error, PopupIcon.Error)
 
 }
 
@@ -76,27 +85,24 @@ function CreditCardValidator($, document, url)
 
 CreditCardValidator.prototype.validate = function () 
 {
-    let creditCardNo = this.document.getElementById("cardNo").value.replace(/\s+/g, '');
     let self = this;
-    let span = self.document.getElementById("message");     
+    let creditCardNo = self.document.getElementById("cardNo").value.replace(/\s+/g, '');
 
     if (creditCardNo === "")
     {
-        isEmptyMessage(span);
-        hideIsEmptyMessage(span);
+        popUpMessage(PopupTitle.Empty, ValidationMessage.Empty, PopupIcon.Empty);
         return false;
     }
-
     $("#wait").css("display", "block");
 
     $.ajax({
-        url: this.url,
+        url: self.url,
         type: 'POST',
         contentType: 'application/json',
         data: JSON.stringify(creditCardNo),
-        success: function (response) {
-                            
-            updateResponse(span, response);    
+        success: function (response)
+        {
+            updateResponse(response);
             $("#wait").css("display", "none");
         },
         error: function (jqXHR, textStatus, errorThrown)
@@ -105,45 +111,20 @@ CreditCardValidator.prototype.validate = function ()
     });
 }
 
-CreditCardValidator.prototype.clear = function() 
+CreditCardValidator.prototype.clear = function () 
 {
     let self = this;
-
-    let span = self.document.getElementById("message");
-    span.style.display = "none";
-    span.style.background = "";
-    span.innerHTML = "";
-
     self.document.getElementById("cardNo").value = "";
 }
 
-function isEmptyMessage(span)
-{
-    span.style.background = RESPONSE_COLOR.INVALID;
-    span.innerHTML = VALIDATION_MESSAGE.EMPTY;
-    span.style.display = "flex";
- 
-}
-
-function hideIsEmptyMessage(span)
-{
-    setTimeout(function ()
-    {
-        span.style.display = "none";
-        span.style.background = "";
-        span.innerHTML = "";
-    }, VALIDATION_MESSAGE_TIMEOUT.EMPTY_INPUT);
-}
-
-
 $(document).ready(function ()
 {
-    var validator = new CreditCardValidator($, document);    
-    $('#verify').click(function (e) {
+    var validator = new CreditCardValidator($, document);
+    $('#verify').click(function (e)
+    {
         e.preventDefault();
         validator.validate();
     });
-
     $('#clear').click(function (e)
     {
         e.preventDefault();
